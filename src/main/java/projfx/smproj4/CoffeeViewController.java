1ef46621629e69d0785f541a740877aa75302618
MichaelAaronMcMahon <michaelaaronmcmahon@gmail.com>
2024-03-23 14:11:14 -0400

    fxml files and controllers

---
+++ src/main/java/projfx/smproj4/CoffeeViewController.java
@@ -0,0 +1,46 @@
+
+package projfx.smproj4;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class CoffeeViewController {
+    private HelloController helloController;
+    private Stage stage;
+    private Scene primaryScene;
+    private Stage primaryStage;
+
+    public void initialize() {
+
+    }
+
+    public void setMainController (HelloController controller,
+                                   Stage stage,
+                                   Stage primaryStage,
+                                   Scene primaryScene) {
+        helloController = controller;
+        this.stage = stage;
+        this.primaryStage = primaryStage;
+        this.primaryScene = primaryScene;
+    }
+
+    @FXML
+    public void displayMain() {
+        //stage.close();
+        primaryStage.setScene(primaryScene);
+        primaryStage.show();
+    }
+
+
+}
--- src/main/java/projfx/smproj4/HelloController.java
+++ src/main/java/projfx/smproj4/HelloController.java
@@ -2,13 +2,123 @@ package projfx.smproj4;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    private Stage primaryStage;
+    private Scene primaryScene;
+
+    public void setPrimaryStage(Stage stage, Scene scene) {
+        primaryStage = stage;
+        primaryScene = scene;
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void displayDonutView() {
+        Stage view1 = new Stage();
+        BorderPane root;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("donut-view.fxml"));
+            root = (BorderPane) loader.load();
+            Scene scene = new Scene(root, 500, 400);
+            primaryStage.setScene(scene);
+            DonutViewController donutViewController = loader.getController();
+            donutViewController.setMainController(this, view1, primaryStage, primaryScene);
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR");
+            alert.setHeaderText("Loading View1.fxml.");
+            alert.setContentText("Couldn't load View1.fxml.");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    protected void displayCoffeeView() {
+        Stage view1 = new Stage();
+        BorderPane root;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("coffee-view.fxml"));
+            root = (BorderPane) loader.load();
+            Scene scene = new Scene(root, 500, 400);
+            primaryStage.setScene(scene);
+            CoffeeViewController coffeeViewController = loader.getController();
+            coffeeViewController.setMainController(this, view1, primaryStage, primaryScene);
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR");
+            alert.setHeaderText("Loading View1.fxml.");
+            alert.setContentText("Couldn't load View1.fxml.");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    protected void displaySandwichView() {
+        Stage view1 = new Stage();
+        BorderPane root;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("sandwich-view.fxml"));
+            root = (BorderPane) loader.load();
+            Scene scene = new Scene(root, 500, 400);
+            primaryStage.setScene(scene);
+            SandwichViewController sandwichViewController = loader.getController();
+            sandwichViewController.setMainController(this, view1, primaryStage, primaryScene);
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR");
+            alert.setHeaderText("Loading View1.fxml.");
+            alert.setContentText("Couldn't load View1.fxml.");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    protected void displayAllOrdersView() {
+        Stage view1 = new Stage();
+        BorderPane root;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("all-orders-view.fxml"));
+            root = (BorderPane) loader.load();
+            Scene scene = new Scene(root, 500, 400);
+            primaryStage.setScene(scene);
+            AllOrdersViewController allOrdersViewController = loader.getController();
+            allOrdersViewController.setMainController(this, view1, primaryStage, primaryScene);
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR");
+            alert.setHeaderText("Loading View1.fxml.");
+            alert.setContentText("Couldn't load View1.fxml.");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    protected void displayCurrentOrderView() {
+        Stage view1 = new Stage();
+        BorderPane root;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("current-order-view.fxml"));
+            root = (BorderPane) loader.load();
+            Scene scene = new Scene(root, 500, 400);
+            primaryStage.setScene(scene);
+            CurrentOrderViewController currentOrderViewController = loader.getController();
+            currentOrderViewController.setMainController(this, view1, primaryStage, primaryScene);
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR");
+            alert.setHeaderText("Loading View1.fxml.");
+            alert.setContentText("Couldn't load View1.fxml.");
+            alert.showAndWait();
+        }
     }
 }
