8aecdde83d645ab4a4e2cbbbff045026eab8d643
Steven Rodriguez <rodriguezbrito94@gmail.com>
2024-03-24 15:34:33 -0400

    added functionality for sandwich and coffee

--- src/main/java/projfx/smproj4/Coffee.java
+++ src/main/java/projfx/smproj4/Coffee.java
@@ -2,9 +2,40 @@ package projfx.smproj4;
 
 public class Coffee extends MenuItem{
 
+    private CoffeeAddin [] coffeeAddins;
+    private CoffeeCupSize cupSize;
+    private int quantity;
+    private int index;
 
+    public Coffee(CoffeeCupSize cupSize, int quantity){
+        super();
+        this.coffeeAddins = new CoffeeAddin[5];
+        this.cupSize = cupSize;
+        this.quantity = quantity;
+        this.index = 0;
+    }
+
+    public boolean addAddin(CoffeeAddin addin){
+
+        if (index == 5){//I will delete the print statement later, just have it for testing
+            System.out.println("addin list is full");
+            return false;
+        }
+        for (CoffeeAddin coffeeAddin:coffeeAddins){
+            if (coffeeAddin == addin){
+                return false;
+            }
+        }
+        coffeeAddins[index] = addin;
+        index++;
+        return true;
+    }
     @Override
     public double price() {
-        return 0;
+
+        double cupPriceMultiplier = this.cupSize.ordinal() * 0.50;
+        double addinMultiplier = this.index * 0.30;
+
+        return (cupPriceMultiplier + addinMultiplier + 1.99) * quantity;
     }
 }
--- src/main/java/projfx/smproj4/Sandwich.java
+++ src/main/java/projfx/smproj4/Sandwich.java
@@ -1,8 +1,50 @@
 package projfx.smproj4;
 
 public class Sandwich extends MenuItem{
+
+    private SandwichOption option;
+    private SandwichBread bread;
+    private SandwichAddon [] addons;
+    private int quantity;
+    private int index;
+
+    public Sandwich(SandwichOption option, SandwichBread bread, int quantity){
+        super();
+        this.option = option;
+        this.bread = bread;
+        this.addons = new SandwichAddon[4];
+        this.quantity = quantity;
+        this.index = 0;
+    }
+    public boolean addAddon(SandwichAddon addon){
+
+        if (index == 4){//I will delete the print statement later, just have it for testing
+            System.out.println("addon list is full");
+            return false;
+        }
+        for (SandwichAddon sandwichAddon:addons){
+            if (sandwichAddon == addon){
+                return false;
+            }
+        }
+        addons[index] = addon;
+        index++;
+        return true;
+    }
+
     @Override
     public double price() {
-        return 0;
+
+        double addonMultiplier = 0;
+
+        for (SandwichAddon sandwichAddon:addons){
+            if (sandwichAddon.equals(SandwichAddon.valueOf("Cheese"))){
+                addonMultiplier ++;
+            }
+            else{
+                addonMultiplier += 0.3;
+            }
+        }
+        return (8.99 + this.option.ordinal() + addonMultiplier) * quantity;
     }
 }
