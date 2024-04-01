41aacbf7670745ff4f5b5b7a633746afb7702d5d
Steven Rodriguez <rodriguezbrito94@gmail.com>
2024-04-01 16:41:05 -0400

    Added grow to order and spaced out the program a bit for clarity

--- src/main/java/projfx/smproj4/Order.java
+++ src/main/java/projfx/smproj4/Order.java
@@ -11,7 +11,7 @@ public class Order {
 
     public Order(int orderNumber){
         this.orderNumber = orderNumber;
-        this.menuList = new MenuItem[10];
+        this.menuList = new MenuItem[1];
     }
 
     public int getAddIndex() {
@@ -35,12 +35,16 @@ public class Order {
             }
             this.menuList = menuGrow;
         }*/
+        if (this.addIndex == this.menuList.length){
+            grow();
+        }
         menuItem.setID(this.menuItemId);
         this.menuItemId ++;
         menuList[this.addIndex] = menuItem;
         this.addIndex ++;
         //printMenuList();
         //System.out.println("\n");
+
     }
     public void add(MenuItem[] menuItems){
         for (MenuItem menuItem:menuItems)
@@ -48,23 +52,41 @@ public class Order {
     }
 
     public void remove(MenuItem menuItem){
+
         int removeID = menuItem.getID();
-        for(int i=0; i<this.addIndex; i++){
+
+        for(int i = 0; i < this.addIndex; i++){
+
             if (this.menuList[i].getID() == removeID){
                 this.menuList[i] = null;
-                //if(i>0){
-                for(int j=(i+1); j<this.addIndex; j++){
-                    this.menuList[j-1] = this.menuList[j];
+
+                for(int j = (i + 1); j < this.addIndex; j ++){
+                    this.menuList[j - 1] = this.menuList[j];
                     this.menuList[j] = null;
                 }
-                //}
+
                 this.addIndex --;
 
             }
 
         }
     }
+    /**
+     * Creates a new array with a length 1 space longer and copies the original array to it.
+     * It does this when the original array is full.
+     */
+    private void grow(){
 
+        MenuItem [] replacement = new MenuItem[menuList.length + 1];
+
+        int i = 0;
+        for(MenuItem item:menuList){
+            replacement[i] = item;
+            i ++;
+        }
+        this.menuList = replacement;
+
+    } //helper method to increase the capacity by 1
     public void printMenuList(){
         for (int i=0; i<this.addIndex; i++){
             System.out.println(this.menuList[i].toString());
